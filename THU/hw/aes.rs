68bed40c80728213ee5d206df4a640eabc1d5b9b
DCMMC <xwt97294597@gmail.com>
2021-03-10 20:15:08 +0800

    crypto lab1 t2

--- THU/hw/aes.rs
+++ THU/hw/aes.rs
@@ -1,9 +1,10 @@
-use std::io;
+// use std::io;
 
-static BLOCK_SIZE: usize = 16;
-static ROW_COUNT: usize = 4;
-static PAD_BYTE: u8 = 0;
-static SBOX: [[u8; 16]; 16]  = [
+const BLOCK_SIZE: usize = 16;
+const ROW_COUNT: usize = 4;
+const PAD_BYTE: u8 = 0;
+const ROUND: usize = 10;
+const SBOX: [[u8; 16]; 16]  = [
 [0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76],
 [0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0],
 [0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15],
@@ -20,7 +21,7 @@ static SBOX: [[u8; 16]; 16]  = [
 [0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E],
 [0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF],
 [0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]];
-static INV_SBOX: [[u8; 16]; 16] = [
+const INV_SBOX: [[u8; 16]; 16] = [
 [0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB],
 [0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB],
 [0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E],
@@ -37,6 +38,11 @@ static INV_SBOX: [[u8; 16]; 16] = [
 [0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF],
 [0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61],
 [0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D]];
+const RCON: [u8; 32] = [
+    0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
+    0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
+    0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
+    0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39];
 
 fn pad(states: &mut Vec<u8>) -> () {
     let mut pad_size = states.len() % BLOCK_SIZE;
@@ -254,6 +260,39 @@ fn inv_mix_columns(states: &mut Vec<u8>) {
     }
 }
 
+fn sub_byte(b: u8) -> u8 {
+    INV_SBOX[(b >> 4) as usize][(b & 0x0f) as usize]
+}
+
+fn key_schedule(cipher_key: &[u8; BLOCK_SIZE]) -> [u8; (ROUND + 1) * BLOCK_SIZE] {
+    let mut round_keys = [0u8; (ROUND + 1) * BLOCK_SIZE];
+    for i in 0..BLOCK_SIZE {
+        round_keys[i] = cipher_key[i];
+    }
+    for i in 1..(ROUND + 1) {
+        // last 4 bytes (one word), i.e., last column
+        let mut temp_offset: usize = i * 16 - 4;
+        // last round (16 bytes), i.e., last round key
+        let mut last_round: usize = (i - 1) * 16;
+        // RotByte is cyclic right shift, e.g., (a, b, c, d) => (b, c, d, a)
+        round_keys[i * 16] = sub_byte(round_keys[temp_offset + 1]) ^ RCON[i] ^ round_keys[last_round + 1];
+        round_keys[i * 16 + 1] = sub_byte(round_keys[temp_offset + 2]) ^ round_keys[last_round + 2];
+        round_keys[i * 16 + 2] = sub_byte(round_keys[temp_offset + 3]) ^ round_keys[last_round + 3];
+        round_keys[i * 16 + 3] = sub_byte(round_keys[temp_offset]) ^ round_keys[last_round];
+
+        for j in 1..4 {
+            temp_offset += 4;
+            last_round += 4;
+            round_keys[i * 16 + j * 4] = round_keys[last_round] ^ round_keys[temp_offset];
+            round_keys[i * 16 + j * 4 + 1] = round_keys[last_round + 1] ^ round_keys[temp_offset + 1];
+            round_keys[i * 16 + j * 4 + 2] = round_keys[last_round + 2] ^ round_keys[temp_offset + 2];
+            round_keys[i * 16 + j * 4 + 3] = round_keys[last_round + 3] ^ round_keys[temp_offset + 3];
+        }
+    }
+
+    round_keys
+}
+
 fn print_blocks(states: &mut Vec<u8>) -> () {
     let blocks = states.chunks(BLOCK_SIZE);
     for (num, block) in blocks.enumerate() {
@@ -273,6 +312,7 @@ fn print_block(block: &[u8]) -> () {
 }
 
 fn test_t1(plaintext: &str) -> () {
+    println!("\nTest T1\n");
     println!("plaintext: {}", plaintext);
     let mut plain_bytes: Vec<u8> = plaintext.to_string()
         .into_bytes();
@@ -297,13 +337,12 @@ fn test_t1(plaintext: &str) -> () {
 }
 
 fn test_t2(plaintext: &str) -> () {
+    println!("\nTest T2\n");
     println!("plaintext: {}", plaintext);
     let mut padded_bytes: Vec<u8> = plaintext.to_string()
         .into_bytes();
     let states: &mut Vec<u8> = &mut padded_bytes;
     pad(states);
-    println!("Pad:");
-    print_blocks(states);
 
     println!("Shift Rows:");
     shift_rows(states);
@@ -321,24 +360,46 @@ fn test_t2(plaintext: &str) -> () {
     inv_shift_rows(states);
     print_blocks(states);
 
-    println!("Unpad:");
     unpad(states);
     let result_text = String::from_utf8(states.to_vec())
         .unwrap();
     println!("Result text: {}", result_text);
 }
 
+fn test_t3(cipher_key: &str) -> () {
+    println!("\nTest T3\n");
+    let key_bytes = cipher_key.to_string().into_bytes();
+    assert_eq!(cipher_key.len(), BLOCK_SIZE);
+    let mut key_bytes_vec = [0u8; BLOCK_SIZE];
+    for (i, &b) in key_bytes.iter().enumerate() {
+        key_bytes_vec[i] = b;
+    }
+    let round_keys = key_schedule(&key_bytes_vec);
+    println!("Round keys generated from {}", cipher_key);
+    for i in 0..(ROUND + 1) {
+        println!("Round {}: {:?}", i, &round_keys[(i * BLOCK_SIZE)..((i + 1) * BLOCK_SIZE)]);
+    }
+}
+
 fn main() -> () {
     println!("AES Lab");
     let mut plaintext = String::new();
-    println!("Please enter the plaintext:");
 
-    io::stdin()
-        .read_line(&mut plaintext)
-        .expect("Failed to read plaintext");
+    // println!("Please enter the plaintext:");
+    //
+    // io::stdin()
+    //     .read_line(&mut plaintext)
+    //     .expect("Failed to read plaintext");
+    //
+    // plaintext = plaintext.trim_end_matches('\n').to_string();
 
-    plaintext = plaintext.trim_end_matches('\n').to_string();
-
+    plaintext.push_str("abcdefghijklmn");
     test_t1(&plaintext);
+
+    plaintext.clear();
+    plaintext.push_str("abcdefghijklmnop");
     test_t2(&plaintext);
+
+    let cipher_key = "abcdefghijklmnop";
+    test_t3(cipher_key);
 }
